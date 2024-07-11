1617d48f7f8dffa4eae9320b566ba0d6a9a87fa6
Amit Kumar Sah <32677659+JTM152769@users.noreply.github.com>
2024-07-11 14:58:36 +0530

    Update BestTimeToBuyAndSellStock.java

--- Array/Easy/BestTimeToBuyAndSellStock.java
+++ Array/Easy/BestTimeToBuyAndSellStock.java
@@ -1,1 +1,39 @@
+package Easy;
+
+public class BestTimeToBuyAndSellStock {
+
+    //Bruteforce
+    //TC: O(n2) SC:O(1)
+    static int maxProfit(int[] arr, int n){
+        int maxProfit = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(arr[j] > arr[i]){
+                    maxProfit = Math.max(arr[j]-arr[i], maxProfit);
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    //Optimal approach
+    //TC: O(n) SC: O(1)
+    static int maxProfitOptimal(int[] arr, int n){
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++){
+            minPrice = Math.min(arr[i], minPrice);
+            maxProfit = Math.max(arr[i] - minPrice, maxProfit);
+        }
+        return maxProfit;
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        int n = arr.length;
+        int maxPro = maxProfit(arr,n);
+        System.out.println("Max profit is: " + maxPro);
+    
+    }
+}
 
